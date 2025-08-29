abb5dfedcee32d580f1f09ec56dd1c97e3c54ab8
Alexander Plattel <acpcreation@gmail.com>
2025-08-29 17:17:04 +0000

    Adjusted variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "region_selection" {
+  description = "Region to deploy to"
+  default     = "us-east-1"
+}
